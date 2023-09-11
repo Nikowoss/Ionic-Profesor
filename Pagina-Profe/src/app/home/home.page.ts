78d0ac8386b208b87572fbee2b13fe336a29ab5b
Nico <ni.navarroa@duocuc.cl>
2023-09-11 18:48:49 -0300

    Ya no se ven menu en paginas inicio y recpass

--- Pagina-Profe/src/app/home/home.page.ts
+++ Pagina-Profe/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,45 +9,51 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage {
 
-  usuario={
-    username:'',
-    password:''
-   }
-  
-    constructor(
-      private router:Router,
-      private alertController:AlertController
-      ) { }
-  
-    ngOnInit() {
-    }
-  
-    onSubmit()
-    {
-      if (this.usuario.username=="nico" && this.usuario.password=="nico"){
-        this.router.navigate(['/generar-qr'])
-      }
-      else{
-        
-        this.presentAlert()
-      }
-  
-    }
-  
-  
-    async presentAlert() {
-      const alert = await this.alertController.create({
-        header: 'Alerta',
-        subHeader: 'Información',
-        message: "Correo y/o contraseña incorrectos",
-        buttons: ['OK'],
-        backdropDismiss:false,
-        
-      });
-  
-      await alert.present();
-  
-   
-    }
+  usuario = {
+    username: '',
+    password: ''
   }
-  
+
+  constructor(
+    private router: Router,
+    private alertController: AlertController,
+    public menuCtrl: MenuController
+
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.usuario.username == "nico" && this.usuario.password == "nico") {
+      this.router.navigate(['/generar-qr'])
+    }
+    else {
+
+      this.presentAlert()
+    }
+
+  }
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Información',
+      message: "Correo y/o contraseña incorrectos",
+      buttons: ['OK'],
+      backdropDismiss: false,
+
+    });
+
+    await alert.present();
+
+
+  }
+  ionViewDidEnter() {
+    this.menuCtrl.enable(false)
+  }
+  ionViewWillLeave() {
+    this.menuCtrl.enable(true)
+  }
+}
